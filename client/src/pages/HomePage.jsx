3d3b66f451718efdab902e39cab63572a55a9dc0
Tahaiff <tahaasifaqwe@gmail.com>
2025-01-21 10:17:51 +0500

    Updated

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,6 +4,8 @@ import Home from '../components/Home/Home'
 const HomePage = () => {
   return (
 <>
+    <img src="/homebg.png" className="absolute w-1/3 bottom-0 right-0 opacity-10 animate-pulse"></img>
+    <img src="/homebg.png" className="absolute w-1/3 top-0 left-0 opacity-10 animate-pulse"></img>
 <Home/>
 </>
   )
